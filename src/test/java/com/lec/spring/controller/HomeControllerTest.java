ea559dcb0475ad8a2bc10418adc5351e3e286c53
GiwonzlJon <gkgk7610@gmail.com>
2023-12-14 11:39:25 +0900

    Initial commit

---
+++ src/test/java/com/lec/spring/controller/HomeControllerTest.java
@@ -0,0 +1,29 @@
+package com.lec.spring.controller;
+
+import org.apache.coyote.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HomeControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Value("${server.port}")
+    private String port;
+    @Test
+    void hello(){
+        System.out.println("port : " + port);
+        ResponseEntity<String> response =  restTemplate.getForEntity("/aws/v2", String.class);
+
+        // "<h1>AWS 배포 v1 </h1>" 와 같아야 통과 (한글자라도 틀리면 테스트 페일)
+        assertEquals("<h1>AWS 배포 v2 </h1>", response.getBody());
+    }
+}
